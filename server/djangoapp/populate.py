92127b4301afb7f1f9cac60046ab9b91fad2e568
SofakiM <sofia.marioli@hotmail.com>
2024-08-04 09:14:34 -0400

    Adding temporary changes to Github

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,38 @@
+from .models import CarMake, CarModel
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    car_make_data = [
+        {"name":"NISSAN", "description":"Great cars. Japanese technology", "website":"https://www.nissan.de"},
+        {"name":"Mercedes", "description":"Great cars. German technology", "website":"https://www.mercedes-benz.de"},
+        {"name":"Audi", "description":"Great cars. German technology", "website":"https://www.audi.de"},
+        {"name":"Kia", "description":"Great cars. Korean technology", "website":"https://www.kia.com"},
+        {"name":"Toyota", "description":"Great cars. Japanese technology", "website":"https://www.toyota.de"},
+    ]
+
+    car_make_instances = []
+    for data in car_make_data:
+            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description'], website=data['website']))
+
+
+    # Create CarModel instances with the corresponding CarMake instances
+    car_model_data = [
+      {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0], "fuel_consumption": "23 MPG"},
+      {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0], "fuel_consumption":"23 MPG"},
+      {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":car_make_instances[0], "fuel_consumption":"20 MPG"},
+      {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1], "fuel_consumption":"19 MPG"},
+      {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1], "fuel_consumption":"25 MPG"},
+      {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1], "fuel_consumption":"23 MPG"},
+      {"name":"A4", "type":"SUV", "year": 2023, "car_make":car_make_instances[2], "fuel_consumption":"21 MPG"},
+      {"name":"A5", "type":"SUV", "year": 2023, "car_make":car_make_instances[2], "fuel_consumption":"15 MPG"},
+      {"name":"A6", "type":"SUV", "year": 2023, "car_make":car_make_instances[2], "fuel_consumption":"17 MPG"},
+      {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":car_make_instances[3], "fuel_consumption":"25 MPG"},
+      {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":car_make_instances[3], "fuel_consumption":"18 MPG"},
+      {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":car_make_instances[3], "fuel_consumption":"22 MPG"},
+      {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4], "fuel_consumption":"23 MPG"},
+      {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4], "fuel_consumption":"20 MPG"},
+      {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":car_make_instances[4], "fuel_consumption":"15 MPG"},
+        # Add more CarModel instances as needed
+    ]
+
+    for data in car_model_data:
+            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'], fuel_consumption=['fuel_consumption'])
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://sofiamarioli-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',]
-CSRF_TRUSTED_ORIGINS=['https://sofiamarioli-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',]
+ALLOWED_HOSTS=['localhost','https://sofiamarioli-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',]
+CSRF_TRUSTED_ORIGINS=['https://sofiamarioli-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -90,6 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'PASSWORD': 'MR4nwYUkpFwv8qLGZEHt5GxY',
     }
 }
 
